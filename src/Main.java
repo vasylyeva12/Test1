f4f12a51618ac321e7406e76a845bf496b9b475d
vasylyeva12 <aloyna.vasilieva@gmail.com>
2022-12-11 19:03:48 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+      File file = new File("src/text.txt");
+      Scanner scanner = new Scanner(file);
+      System.out.println(scanner.nextLine());
+      System.out.println(scanner.nextLine());
+      System.out.println(scanner.nextLine());
+
+
+//        String name = "Alona";
+//        Integer age = 46;
+///       Float height = 184.6F;
+//        Boolean student = true;
+//
+//      Scanner scanner = new Scanner(System.in);
+//      System.out.println("Укажіть ваше ім'я ");
+//      String name = scanner.nextLine();
+//        System.out.println("Hello " + name);
+//
+//        System.out.println("Укажіть ваш вік ");
+//        Integer age = scanner.nextInt();
+//
+//        System.out.println("age " + age);
+//
+//        System.out.println("Ви студент? ");
+//        Boolean isStudent = scanner.nextBoolean();
+//        System.out.println("isStudent " + isStudent);
+
+//        System.out.println("height: " + height);
+//
+//        String value_1 = "13";
+//        String value_2 = "23";
+//        System.out.println(Integer.parseInt(value_1) + Integer.parseInt(value_2));
+
+    }
+}
---
+++ src/lesson2.java
@@ -0,0 +1,32 @@
+public class lesson2 {
+    public static void main(String[] args) {
+        //Task 1
+
+        int x = 54;
+        int y = 16;
+        int result = x + y;
+
+        System.out.println("Result = " + result);
+
+        // Task 2
+        double z = 50;
+        double n = 3;
+        System.out.println("Result =" + (z/n));
+
+        // Task 3
+
+        int h = -5;
+        int f = 8;
+        int s = 6;
+        System.out.println("Result = " + (h+f+s));
+
+        int y1 = x*x;
+        x = y1;
+        x = 5;
+        System.out.println(y1);
+
+
+
+
+    }
+}
